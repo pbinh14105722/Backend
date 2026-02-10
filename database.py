cd8b371636ad79be9eaddc91596b2431582614a8
pbinh14105722 <thachdaupro9@gmail.com>
2026-02-10 16:17:11 +0700

    change database
    Updated database connection URL to include SSL mode for Render compatibility.

--- database.py
+++ database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from fastapi.security import OAuth2PasswordBearer
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:binh14105722@db.quarzvpkrjjdhhnekwka.supabase.co:5432/postgres"
+# Tôi đã thêm ?sslmode=require vào cuối link để Render không chặn kết nối
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.quarzvpkrjjdhhnekwka:binh14105722@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres?sslmode=require"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -11,7 +12,6 @@ Base = declarative_base()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-# Dependency để lấy DB session cho mỗi request
 def get_db():
     db = SessionLocal()
     try:

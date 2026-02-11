49531dcb67c5e60c408e6647368905add7996f59
pbinh14105722 <thachdaupro9@gmail.com>
2026-02-11 19:48:44 +0700

    UPDATE

--- models.py
+++ models.py
@@ -1,5 +1,16 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 from database import Base
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(String, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    type = Column(String)  # "FOLDER" hoặc "PROJECT"
+    parent_id = Column(String, ForeignKey("items.id", ondelete="CASCADE"), nullable=True)
+    position = Column(Integer, default=0)
+    color = Column(String, default="#ffffff")
+    expanded = Column(Boolean, default=False)
 
 class User(Base):
     __tablename__ = "users"

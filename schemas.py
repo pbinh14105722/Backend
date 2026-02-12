e9928a09c75c988210a936e17443ddecbd71ad0b
pbinh14105722 <thachdaupro9@gmail.com>
2026-02-12 17:16:27 +0700

    Debug

--- schemas.py
+++ schemas.py
@@ -29,13 +29,23 @@ class UserCreate(BaseModel):
     email: EmailStr
     password: str
 
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
 class UserResponse(BaseModel):
     id: int
     email: EmailStr
     class Config:
         from_attributes = True
 
+class AuthResponse(BaseModel):
+    message: str
+    access_token: str
+    token_type: str
+    # Bạn có thể thêm trường user nếu muốn frontend hiển thị tên người dùng
+    # username: Optional[str] = None
+
 class Token(BaseModel):
     access_token: str
     token_type: str
-

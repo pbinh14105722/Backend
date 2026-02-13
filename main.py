807374ca1a999ea025f0ea8ba5e0139821ecd27d
pbinh14105722 <thachdaupro9@gmail.com>
2026-02-13 21:34:21 +0700

    Debug

--- main.py
+++ main.py
@@ -94,23 +94,16 @@ def get_user_from_token(db: Session, email: str):
         raise HTTPException(status_code=404, detail="User không tồn tại")
     return user
 
-# 1. Lấy danh sách (CHỈ LẤY CỦA MÌNH)
-@app.get("/items", response_model=list[schemas.ItemResponse])
-def get_my_items(
-    db: Session = Depends(database.get_db), 
-    current_user: models.User = Depends(get_current_user)
-):
-    return db.query(models.Item).filter(models.Item.owner_id == current_user.id).order_by(models.Item.position.asc()).all()
+# 1. Lấy toàn bộ danh sách (Sắp xếp theo position)
+@app.get("/data")#, response_model=list[schemas.ItemResponse])
+def get_all_items(db: Session = Depends(database.get_db), current_user: str = Depends(get_current_user)):
+    #return db.query(models.Item).order_by(models.Item.position.asc()).all()
+    return db.query(models.Item).filter(models.Item.owner_email == current_user).order_by(models.Item.position.asc()).all()
 
-# 2. Thêm mới (TỰ ĐỘNG GÁN CHO MÌNH)
+# 2. Thêm mới một item
 @app.post("/items", response_model=schemas.ItemResponse)
-def create_item(
-    item: schemas.ItemCreate, 
-    db: Session = Depends(database.get_db), 
-    current_user: models.User = Depends(get_current_user)
-):
-    # Tạo item mới và gán owner_id là id của user đang đăng nhập
-    db_item = models.Item(**item.model_dump(), owner_id=current_user.id)
+def create_item(item: schemas.ItemCreate, db: Session = Depends(database.get_db), current_user: str = Depends(get_current_user)):
+    db_item = models.Item(**item.model_dump())
     db.add(db_item)
     db.commit()
     db.refresh(db_item)

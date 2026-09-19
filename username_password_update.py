012015c6e91bec35a67a41f55bd758d07e207f7b
pbinh14105722 <thachdaupro9@gmail.com>
2026-09-19 15:00:13 +0700

    Refactor authentication by removing get_current_user
    Removed the get_current_user function and its dependencies.

--- username_password_update.py
+++ username_password_update.py
@@ -1,36 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
-from jose import jwt, JWTError
 import models, utils, database, schemas
-from utils import SECRET_KEY, ALGORITHM
+from dependencies import get_current_user
 
 router = APIRouter()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-
-
-def get_current_user(
-    db: Session = Depends(database.get_db),
-    token: str = Depends(oauth2_scheme)
-):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Không thể xác thực thông tin",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email: str = payload.get("sub")
-        if email is None:
-            raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-
-    user = db.query(models.User).filter(models.User.email == email).first()
-    if user is None:
-        raise credentials_exception
-    return user
 
 
 class UpdateUsername(BaseModel):
